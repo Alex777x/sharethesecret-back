362f4f2f10f1a20b5a46107e363fa753215cb171
Anatoli Aliaksandrou <alexandroff103@gmail.com>
2025-05-07 20:09:22 +0200

    add multipart file size limits and health checks in Docker Compose

--- src/main/java/pl/aliaksandrou/sharethesecretback/controller/NoteController.java
+++ src/main/java/pl/aliaksandrou/sharethesecretback/controller/NoteController.java
@@ -2,9 +2,11 @@ package pl.aliaksandrou.sharethesecretback.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import pl.aliaksandrou.sharethesecretback.model.NoteData;
 import pl.aliaksandrou.sharethesecretback.model.NoteRequest;
@@ -53,5 +55,12 @@ public class NoteController {
             String base64 = Base64.getEncoder().encodeToString(note.getEncryptedData());
             return ResponseEntity.ok(Map.of("content", base64));
         }
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity
+                .status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(Map.of("error", "File size exceeds the maximum allowed size of 2MB"));
     }
 }
